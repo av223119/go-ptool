b4de3ef99c9bf986045044d3d54082967a887de2
Alexandr Vlasov <alexander.vlasov@gmail.com>
2024-04-02 22:49:12 +0200

    Nocam just repeats the filename for the moment

--- cmd/nocam.go
+++ cmd/nocam.go
@@ -2,15 +2,40 @@ package cmd
 
 import (
 	"fmt"
+	"slices"
+	"strings"
+
+	"github.com/av223119/go-ptool/internal"
 
 	"github.com/spf13/cobra"
 )
 
+func collector(input <-chan string, output chan<- string) {
+	defer close(output)
+	res := []string{}
+	for s := range input {
+		res = append(res, s)
+	}
+	slices.Sort(res)
+	output <- strings.Join(res, "\n")
+}
+
+func worker(p string) string {
+	return p
+}
+
 var nocamCmd = &cobra.Command{
 	Use:   "nocam",
 	Short: "Find files without camera maker/model data",
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("nocam called")
+		txt, err := internal.Dispatcher(
+			worker,
+			collector,
+			args[0],
+		)
+		cobra.CheckErr(err)
+		fmt.Println(txt)
 	},
 }
 
