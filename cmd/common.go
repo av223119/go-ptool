107275019b57c22682342ab8189e6ce653330a2d
Alexandr Vlasov <alexander.vlasov@gmail.com>
2024-05-26 00:51:30 +0200

    nogpsdir implemented

--- cmd/common.go
+++ cmd/common.go
@@ -1,13 +1,35 @@
 package cmd
 
 import (
-	"slices"
+	"fmt"
+	"path"
+	"sort"
 	"strings"
 )
 
 type FileBool struct {
 	Filename string
 	Result bool
+}
+
+type boolCounter map[bool]uint
+type mapCounter map[string]boolCounter
+
+func (m mapCounter) String() string {
+	keys := []string{}
+	for k, v := range m {
+		if v[true] != 0 {
+			keys = append(keys, k)
+		}
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		return m[keys[i]][true] < m[keys[j]][true]
+	})
+	res := make([]string, len(keys))
+	for i, k := range(keys) {
+		res[i] = fmt.Sprintf("%3v / %3v  %v", m[k][true], m[k][false], k)
+	}
+	return strings.Join(res, "\n")
 }
 
 func listCollector(input <-chan FileBool, output chan<- string) {
@@ -18,6 +40,19 @@ func listCollector(input <-chan FileBool, output chan<- string) {
 			res = append(res, s.Filename)
 		}
 	}
-	slices.Sort(res)
+	sort.Strings(res)
 	output <- strings.Join(res, "\n")
+}
+
+func dirCollector(input <-chan FileBool, output chan<- string) {
+	defer close(output)
+	res := mapCounter{}
+	for s := range input {
+		dirName := path.Dir(s.Filename)
+		if res[dirName] == nil {
+			res[dirName] = boolCounter{}
+		}
+		res[dirName][s.Result] += 1
+	}
+	output <- res.String()
 }
---
+++ cmd/nogpsdir.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/av223119/go-ptool/internal"
+)
+
+var nogpsDirCmd = &cobra.Command{
+	Use:   "nogpsdir",
+	Short: "Files without GPS data, per directory",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		txt, err := internal.Dispatcher(
+			nogpsWorker,
+			dirCollector,
+			args[0],
+		)
+		cobra.CheckErr(err)
+		fmt.Println(txt)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(nogpsDirCmd)
+}
