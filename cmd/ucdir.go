c3e2a0e7a8df4f9c8c619c1d45e412a49b41f80f
Alexandr Vlasov <alexander.vlasov@gmail.com>
2026-05-14 13:28:09 +0200

    Two more usercomment-related commands

---
+++ cmd/ucdir.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+
+	"github.com/av223119/go-ptool/internal"
+)
+
+func ucboolWorker(p string) (FileBool, error) {
+	x, err := internal.ParseFile(p)
+	if err != nil {
+		log.Fatalln(err)
+		return FileBool{p, false}, err
+	}
+	if x.ExifIFD.UserComment != "" {
+		return FileBool{p, true}, nil
+	}
+	return FileBool{p, false}, nil
+}
+
+func init() {
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "ucdir",
+		Short: "Count files with UserComment, per directory",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			txt, err := internal.Dispatcher(
+				isImage,
+				ucboolWorker,
+				dirCollector,
+				args[0],
+				exclude,
+			)
+			cobra.CheckErr(err)
+			fmt.Println(txt)
+		},
+	})
+}
---
+++ cmd/ucstat.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+
+	"github.com/av223119/go-ptool/internal"
+)
+
+func ucstatWorker(p string) (string, error) {
+	x, err := internal.ParseFile(p)
+	if err != nil {
+		log.Fatalln(err)
+		return "parse_error", err
+	}
+	if x.ExifIFD.UserComment != "" {
+		return "present", nil
+	}
+	return "absent", nil
+}
+
+func init() {
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "ucstat",
+		Short: "Overall UserComment statistics",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			txt, err := internal.Dispatcher(
+				isImage,
+				ucstatWorker,
+				tableCollector,
+				args[0],
+				exclude,
+			)
+			cobra.CheckErr(err)
+			fmt.Println(txt)
+		},
+	})
+}
